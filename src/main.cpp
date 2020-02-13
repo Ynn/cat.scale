d802b53d0a99796ac4d39507f238db2604f55f77
ynn <ynn@mel.yt>
2020-02-13 11:16:58 +0100

    try to improve battery

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 #define wifi_ssid "SUSAN_nomap"
 #define wifi_password "celui qui ne pense pas a moi est un egoiste"
 
-#define mqtt_server "hairdresser.cloudmqtt.com"
-#define mqtt_user "oipayvjr"  //s'il a été configuré sur Mosquitto
-#define mqtt_password "57zb4p0F7R7n" //idem
+#define mqtt_server "54.154.107.222"
+#define mqtt_user "oipayvjr"  
+#define mqtt_password "57zb4p0F7R7n"
 #define mqtt_port 16798
 #define cat_topic "cat/bowl"
 
@@ -31,20 +31,20 @@ HX711 scale;
 //Connexion au réseau WiFi
 void setup_wifi() {
   delay(10);
-  Serial.println();
-  Serial.print("Connect to");
-  Serial.println(wifi_ssid);
+  // Serial.println();
+  // Serial.print("Connect to");
+  // Serial.println(wifi_ssid);
 
   WiFi.begin(wifi_ssid, wifi_password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    // Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("Wifi ok ");
-  Serial.print("=> IP : ");
+  // Serial.println("");
+  // Serial.println("Wifi ok ");
+  // Serial.print("=> IP : ");
   Serial.print(WiFi.localIP());
 }
 
@@ -52,56 +52,86 @@ void setup_wifi() {
 void reconnect() {
   //Boucle jusqu'à obtenur une reconnexion
   while (!client.connected()) {
-    Serial.print("MQTT...");
+    // Serial.print("MQTT...");
     if (client.connect("Cat", mqtt_user, mqtt_password)) {
-      Serial.println("OK");
+      // Serial.println("OK");
     } else {
-      Serial.print("KO, error : ");
-      Serial.print(client.state());
-      Serial.println(" wait 5s...");
+      // Serial.print("KO, error : ");
+      // Serial.print(client.state());
+      // Serial.println(" wait 5s...");
       delay(5000);
     }
   }
 }
 
-void sendAndSleep(){
-  for (int i = 0; i <= 3; i++) {
 
-    client.loop();
-    while (!client.connected()) 
-    {
-      reconnect();
-    }
 
+/**
+ * return the weight or -1 if the mesure failed
+ ***/
+long mesure(){
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.power_up();
+
+  long adjusted = 100000;
+
+  //Takes the minimum of 3 mesures in 3s.
+  for(int i =0; i< 3; i++){
     if (scale.is_ready()) {
       long reading = scale.read();
-      long adjusted = (reading + B)*A;
-      if(adjusted < 0){
-        adjusted = 0;
+      long new_adjusted = (reading + B)*A;
+      if(new_adjusted < adjusted){
+        adjusted = new_adjusted;
       }
-      Serial.println(adjusted);
-      client.publish(cat_topic, String(adjusted).c_str(), true); 
       delay(1000);
     }
   }
   scale.power_down();			        // put the ADC in sleep mode
 
-  //Sleep for 10 minutes :
-  ESP.deepSleep(10*60* 1000000, RF_DEFAULT);
+  if(adjusted < 0){
+    adjusted = 0;
+  }else{
+    return -1;
+  }
+
+  return adjusted;
+}
+
+
+void sendAndSleep(long mesure){
+
+  // Connect to WIFI
+  setup_wifi();
+
+  //Connect to MQTT
+  client.setServer(mqtt_server, mqtt_port);   
+
+  // TEST MQTT connection
+  client.loop();
+  while (!client.connected()) 
+  {
+    reconnect();
+  }
+
+  client.publish(cat_topic, String(mesure).c_str(), true); 
 
 }
+
 
 void setup() {
-  Serial.begin(MONITOR_SPEED);
-  Serial.print("Starting: ");
-  setup_wifi();      
-  client.setServer(mqtt_server, mqtt_port);   
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.power_up();			        // put the ADC in sleep mode
-  sendAndSleep();
+  // Serial.begin(MONITOR_SPEED);
+  // Serial.print("Starting: ");
+
+  long mesure = mesure();
+  if(mesure > -1){
+    sendAndSleep(mesure);
+    //Sleep for 10 minutes :
+    ESP.deepSleepInstant(10*60*1000000, RF_DEFAULT);
+  }else{
+    //retry in one minute
+    ESP.deepSleepInstant(60*1000000, RF_DEFAULT);
+  }
 }
-
-
 
 void loop() {
 
